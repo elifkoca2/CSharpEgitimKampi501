2a90afd3a67167c49f63d3482df19b162407d65e
elifkoca2 <koca24988@gmail.com>
2025-01-07 12:09:48 +0300

    Add project files.

---
+++ CSharpEgitimKampi501/ConnectionString/DatabaseConncetionString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi501.ConnectionString
+{
+    public static class DatabaseConncetionString
+    {
+        public static void SQLDatabaseConncetionString()
+        {
+            SqlConnection connection = new SqlConnection("Server=DESKTOP-CL0PR4G;initial Catalog=EgitimKampi501Db;integrated security;true");
+        }
+    }
+}
